59bbf4783cf7531ee2caae869e704c90cd71465b
Tan Shi <20091610@mail.wit.ie>
2020-12-30 02:26:54 +0000

    Fix bug and improve favorite function

--- api/users/index.js
+++ api/users/index.js
@@ -12,19 +12,21 @@ router.get('/', (req, res,next) => {
 
 // Register OR authenticate a user
 router.post('/', async (req, res, next) => {
-  if (!req.body.username || !req.body.password) {
-    res.status(401).json({
-      success: false,
-      msg: 'Please pass username and password.',
-    });
-  }
-  if (req.query.action === 'register') {
-    await User.create(req.body).catch(next);
-    res.status(201).json({
-      code: 201,
-      msg: 'Successful created new user.',
-    });
-  } else {
+    if (req.query.action === 'register') {
+      const judge = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+        if(judge.test(req.body.password)){
+        await User.create(req.body).catch(next);
+        res.status(201).json({
+          code: 201,
+          msg: 'Successful created new user.',
+        });
+      }else{
+          res.status(401).json({
+          code: 401,
+          msg: 'Password needs to comply with the rules.',
+          });
+        }
+      } else {
     const user = await User.findByUserName(req.body.username).catch(next);
       if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
       user.comparePassword(req.body.password, (err, isMatch) => {
@@ -59,13 +61,24 @@ router.put('/:id',  (req, res,next) => {
 
 //Add a favourite. No Error Handling Yet. Can add duplicates too!
 router.post('/:userName/favourites', async (req, res, next) => {
+  try{
   const newFavourite = req.body.id;
   const userName = req.params.userName;
   const movie = await movieModel.findByMovieDBId(newFavourite);
   const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
+  if(user.favourites.indexOf(movie._id) === -1){
+    await user.favourites.push(movie._id);
+  }else{
+    res.status(402).json({
+      code: 402,
+      msg: 'Movie exist.',
+      });
+  }
   await user.save(); 
-  res.status(201).json(user); 
+  res.status(201).json(user);
+  }catch(error){
+    next(error);
+  } 
 });
 router.get('/:userName/favourites', (req, res, next) => {
   const userName = req.params.userName;
