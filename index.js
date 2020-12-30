c2f88d3e7306cd139f8ace35cb6e57220f317c1d
Tan Shi <20091610@mail.wit.ie>
2020-12-30 02:14:41 +0000

    Update Movies Data/Route

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
-import './db';
-import {loadUsers} from './seedData'
+import './db';  
 import usersRouter from './api/users';
-import genresRouter from './api/genres'
+import genresRouter from './api/genres';
 import session from 'express-session';
 import passport from './authenticate';
+import {loadUsers, loadMovies} from './seedData';
 
 
 dotenv.config();
@@ -18,6 +18,7 @@ const port = process.env.PORT;
 
 if (process.env.SEED_DB) {
   loadUsers();
+  loadMovies();
 }
 
 const errHandler = (err, req, res, next) => {
