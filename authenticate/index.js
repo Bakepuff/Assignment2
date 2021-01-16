1bbafaef6b2d56f4c70aa7bd778f7e30c2f65122
Tan Shi <20091610@mail.wit.ie>
2021-01-16 03:40:17 +0000

    Fix Authenticate Bug

--- authenticate/index.js
+++ authenticate/index.js
@@ -4,7 +4,7 @@ export default async (req, res, next) => {
   if (req.session && req.session.authenticated) {
     let user = await User.findByUserName(req.session.user);
     if (!user)
-      return res.status(401).json({status:401,message:"unauthorised"});
+    return res.status(401).json({status:401,message:"unauthorised"});
     next();
   } else {
     return res.status(401).json({status:401,message:"unauthorised"});
