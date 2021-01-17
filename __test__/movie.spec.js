fb5c024dbe2564c26736283b0ede6c1728dca669
Tan Shi <20091610@mail.wit.ie>
2021-01-17 11:04:31 +0000

    add function&test

--- __test__/movie.spec.js
+++ __test__/movie.spec.js
@@ -2,7 +2,9 @@ import request from 'supertest'
 import mongoose from 'mongoose'
 import app from '../index'
 
-const baseUrl = '/api/movies'
+const baseUrl = '/api/movie'
+const movieID = 464052
+const title = "Wonder Woman 1984"
 const wrongID = 999999;
 
 const user = {
@@ -11,22 +13,35 @@ const user = {
 }
 
 describe('GET', () => {
-  
-  it('should not get movie list without login', async (done) => {
-    await request(app)
-      .get(baseUrl)
-      .set('authorization', 'fail')
-      .expect(401)
-    done()
-  })
-
-  it('should return 401 when the id is invalid', async (done) => {
+  it('should get movie list', async (done) => {
+      await request(app)
+        .get(baseUrl)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((res) => {
+          expect(res.body.length).toBe(40)
+        })
+      done()
+    })
+  it('should get specfied movie', async (done) => {
+        await request(app)
+          .get(`${baseUrl}/${movieID}`)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.id).toBe(464052)
+            expect(res.body.original_title).toBe(title)
+          })
+        done()
+      })
+  it('should return 404 when the id is invalid', async (done) => {
     await request(app)
       .get(`${baseUrl}/${wrongID}`)
-      .expect(401)
+      .set("Accept", "application/json")
+      .expect(404)
+      .expect('NOT FOUND');
     done()
   })
-
   afterAll(async () => {
     await mongoose.disconnect()
     await app.close()
